9159ae3a227eb2dae554c18e213b369710c9553b
Carlos <c.cabrera@ciencias.unam.mx>
2022-01-16 21:05:47 -0600

    Creado el ruteo al detalle de la factura

--- proyecto-dwf/src/app/app-routing.module.ts
+++ proyecto-dwf/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'category', component: CategoryComponent},
   { path: 'cart', component: CartComponent},
-  { path: 'invoice', component: InvoiceComponent}
+
 ];
 
 @NgModule({
